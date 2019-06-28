a3715288c79e1c080f8c1d171eb3cef4b8df8ddc
Satish chand bhardwaj <satish.cbhardwaj@gmail.com>
2019-06-28 11:23:07 +0530

    remove db url

--- admin/backend/project/startup/db.js
+++ admin/backend/project/startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function(){
-    const uri= 'mongodb+srv://satish:yE3bxbXqnefQG54M@scb-rh5i4.mongodb.net/merisawaari?retryWrites=true';
+    const uri= '';
     mongoose.set('useCreateIndex', true);
     mongoose.connect(uri,{ useNewUrlParser: true })
     .then(() => console.log('connect with mongoDB'))
